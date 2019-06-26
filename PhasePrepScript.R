ef24f1928eb426ca27565c337a5665e6ee49f7ba
sgagliano <gsarah6221@gmail.com>
2019-06-26 19:15:45 -0400

    Update PhasePrepScript.R

--- PhasePrepScript.R
+++ PhasePrepScript.R
@@ -2,9 +2,9 @@
 #This script prepares a bash script with the list of eagle phasing jobs
 
 DIR <- "<path to directory where the bcfs, split by chromosome, to be phased are located>"
-INPUT <- "<path to directory where the genetic map is located, hg38 here>"
+MAP <- "<version of genetic map to use from eagle; e.g. genetic_map_hg38_withX.txt.gz>"
 LOGS <- "<path to directory to write out logs>"
-EAGLE <- "<path to eagle executable>"
+EAGLE <- "<eagle executable>"
 
 options(scipen=999)
 tt=NULL
@@ -14,7 +14,7 @@ for(CHR in 1:22)
 
 OUTPREFIX="<my_Output_prefix"
 
-tt=c(tt,paste("",EAGLE," --geneticMapFile ",INPUT,"/genetic_map_hg38_withX.txt.gz --bcf ",DIR,"/chr",CHR,"_GRCh38.genotypes.inpsyght.bcf --numThreads 8 --outPrefix ",OUTPREFIX,".phased-chr",CHR," > ",LOGS,"/",OUTPREFIX,".phased-chr",CHR,".LOG
+tt=c(tt,paste("",EAGLE," --geneticMapFile ",MAP," --bcf ",DIR,"/chr",CHR,"_GRCh38.genotypes.inpsyght.bcf --numThreads 8 --outPrefix ",OUTPREFIX,".phased-chr",CHR," > ",LOGS,"/",OUTPREFIX,".phased-chr",CHR,".LOG
 
 /usr/cluster/bin/bcftools index -f ",OUTPREFIX,".phased-chr",CHR,".vcf.gz ",sep=""))
 
