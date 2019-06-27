04b6674b4fa1bd1c3ecdb8a2aea73458aaa24284
sgagliano <gsarah6221@gmail.com>
2019-06-26 22:25:26 -0400

    added arguments

--- ConvertSummaryStats2b38.sh
+++ ConvertSummaryStats2b38.sh
@@ -1,3 +1,6 @@
+SUMSTATS=$1 #prefix for summary statistics file, assumes sumstats file ends with .sumstats suffix
+COL=$2 #column number with rsIDs in summary statistics file
+
 #DESCRIPTION
 #Convert summary statistics (with rsID) on b37 to b38
 
@@ -10,12 +13,16 @@ gzip -dc All_20180418.vcf.gz | grep -v "^#" | cut -f 1-5 > dbSNP-5cols.txt
 LC_ALL=C; export LC_ALL
 
 #sort summary stats (b37) and the dbSNP (b38) by rsID
-sort -k2 sumstats.sumstats > sumstats.sumstats-sortbyrs
+sort -k2 ${SUMSTATS}.sumstats > ${SUMSTATS}.sumstats-sortbyrs
 sort -k3 input/dbSNP-5cols.txt > dbSNP-5cols.txt-sortbyrs
 
 #join the sorted fies by rs
-join -1 2 -2 3 sumstats.sumstats-sortbyrs dbSNP-5cols.txt-sortbyrs | sort | uniq > sumstats_dbSNPmerge.sumstats
+join -1 2 -2 ${COL} ${SUMSTATS}.sumstats-sortbyrs dbSNP-5cols.txt-sortbyrs | sort | uniq > ${SUMSTATS}_dbSNPmerge.sumstats
 
 #create text file, header, with the appropriate header
-cat header sumstats_dbSNPmerge.sumstats > sumstats_dbSNPmerge.sumstats-header
-mv sumstats_dbSNPmerge.sumstats-header sumstats_dbSNPmerge.sumstats
+cat header ${SUMSTATS}_dbSNPmerge.sumstats > ${SUMSTATS}_dbSNPmerge.sumstats-header
+mv ${SUMSTATS}s_dbSNPmerge.sumstats-header ${SUMSTATS}_dbSNPmerge.sumstats
+
+#remove intermediate files
+rm dbSNP-5cols.txt-sortbyrs
+rm ${SUMSTATS}_dbSNPmerge.sumstats
