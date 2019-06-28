6e1cb50466eb28b85759fec330033d40ea84b7f5
sgagliano <gsarah6221@gmail.com>
2019-06-28 19:35:30 -0400

    Update Check_rsID_dbSNP_Merge.sh

--- Check_rsID_dbSNP_Merge.sh
+++ Check_rsID_dbSNP_Merge.sh
@@ -11,15 +11,15 @@ COLa=$2 #e.g. 2; column number for summary stats chr from merged file
 COLb=$3 #e.g. 14; column number for dbSNP chr from merged file
 ######
 
-#cut -d " " -f ${COLa} ${INPUT} >  chrA
-#cut -d " " -f ${COLb} ${INPUT} >  chrB
+cut -d " " -f ${COLa} ${INPUT} >  chrA
+cut -d " " -f ${COLb} ${INPUT} >  chrB
 
-#diff chrA chrB | grep -v ">" | grep -v "<" | grep -v "-" | cut -d "c" -f 1 > mismatched_lines.txt
+diff chrA chrB | grep -v ">" | grep -v "<" | grep -v "-" | cut -d "c" -f 1 > mismatched_lines.txt
 
-#while read i; do
-#  echo "Appending rsID for mismatched line $i to mismatch_rsID.txt"
-#  head -n ${i} ${INPUT} | tail -n1 | cut -d " " -f 1 >> mismatch_rsID.txt
-#done <mismatched_lines.txt
+while read i; do
+  echo "Appending rsID for mismatched line $i to mismatch_rsID.txt"
+  head -n ${i} ${INPUT} | tail -n1 | cut -d " " -f 1 >> mismatch_rsID.txt
+done <mismatched_lines.txt
 
 LC_ALL=C; export LC_ALL
 sort mismatch_rsID.txt > mismatch_rsID.txt-sort
@@ -29,8 +29,8 @@ cat ../output/header ${INPUT}-clean > ${INPUT}-clean-head
 mv ${INPUT}-clean-head ${INPUT}-clean
 
 #remove intermediate files
-#rm chrA 
-#rm chrB
-#rm mismatched_lines.txt
-#rm mismatch_rsID.txt-sort
-#rm ${INPUT}-sort
+rm chrA 
+rm chrB
+rm mismatched_lines.txt
+rm mismatch_rsID.txt-sort
+rm ${INPUT}-sort
