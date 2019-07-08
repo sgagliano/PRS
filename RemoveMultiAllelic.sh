8dad6636fb151846c354b5421405f27b4e6b83bd
sgagliano <gsarah6221@gmail.com>
2019-07-08 19:49:49 -0400

    Added -f PASS

--- RemoveMultiAllelic.sh
+++ RemoveMultiAllelic.sh
@@ -16,7 +16,7 @@ output= <path to directory where you want the processed bcfs to be outputted>
 for chr in {1..22} X; do
   (bcftools view --no-version -h $dir/inpsyght.chr${chr}.gtonly.minDP0.filtered.PASS.rehdr.vcf.gz | \
     grep -v "^##contig=<ID=[GNh]" | sed 's/^##contig=<ID=MT/##contig=<ID=chrM/;s/^##contig=<ID=\([0-9XY]\)/##contig=<ID=chr\1/'; \
-  bcftools view --no-version -H -c 2 $dir/inpsyght.chr${chr}.gtonly.minDP0.filtered.PASS.rehdr.vcf.gz) | \
+  bcftools view --no-version -f PASS -H -c 2 $dir/inpsyght.chr${chr}.gtonly.minDP0.filtered.PASS.rehdr.vcf.gz) | \
   bcftools norm --no-version -Ou -m -any | \
   bcftools norm --no-version -Ob -o $output/chr${chr}_GRCh38.genotypes.inpsyght.bcf -d none -f $input/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna && \
   bcftools index -f $output/chr${chr}_GRCh38.genotypes.inpsyght.bcf
