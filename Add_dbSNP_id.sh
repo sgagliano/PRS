7859b0a9236c14da5bb3e0714d267505a8e6be48
sgagliano <gsarah6221@gmail.com>
2019-07-16 10:10:55 -0400

    added delimiter

--- Add_dbSNP_id.sh
+++ Add_dbSNP_id.sh
@@ -2,22 +2,22 @@ chr=$1 #col num for dbSNP chr
 bp=$2  #col num for dbSNP pos
 ref=$3 #col num for dbSNP ref allele
 alt=$4 #col num for dbSNP alt allele
-prefix=$5 #tab-delimited output file (prefix only) from Check_rsID_dbSNP_Merge.sh; e.g. ../output/trait_dbSNPmerge
+prefix=$5 #space-delimited output file (prefix only) from Check_rsID_dbSNP_Merge.sh; e.g. script will add _dbSNPmerge.sumstats-clean suffix
 
 LC_ALL=C; export LC_ALL
 
 #extract dbSNP columns; assumes file is tab-delimited
-cut -f ${chr},${bp},${ref},${alt}  ${prefix}-clean  > ${prefix}-clean_ids 
+cut -d " " -f ${chr},${bp},${ref},${alt}  ../output/${prefix}_dbSNPmerge.sumstats-clean  > ../output/${prefix}_dbSNPmerge.sumstats-clean_ids 
 #add chr prefix to agree with raw data b38 notation
-sed -i -e 's/^/chr/' ${prefix-clean_ids 
+sed -i -e 's/^/chr/' ../output/${prefix}_dbSNPmerge.sumstats-clean_ids 
 #remove header and add ids as header
-touch newcolhead
-echo ids > newcolhead
-tail -n+2 ${prefix}-clean_ids  > ${prefix}-clean_ids -nohead
-paste newcolhead ${prefix}-clean_ids-nohead > ${prefix}-clean_ids 
+touch ../output/newcolhead
+echo ids > ../output/newcolhead
+tail -n+2 ../output/${prefix}_dbSNPmerge.sumstats-clean_ids  > ../output/${prefix}_dbSNPmerge.sumstats-clean_ids-nohead
+paste ../output/newcolhead ../output/${prefix}_dbSNPmerge.sumstats-clean_ids-nohead > ../output/${prefix}_dbSNPmerge.sumstats-clean_ids 
 #change delimiter to space
-perl -p -i -e 's/\t/ /g' ${prefix}-clean_ids 
+perl -p -i -e 's/\t/ /g' ../output/${prefix}_dbSNPmerge.sumstats-clean_ids 
 
 #remove intermediate files
-rm newcolhead
-rm ${prefix}-clean_ids-nohead
+rm ../output/newcolhead
+rm ../output/${prefix}_dbSNPmerge.sumstats-clean_ids-nohead
