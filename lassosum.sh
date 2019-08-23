cdb564dda5247c7caf22033ea4b6f0c47a708f5b
sgagliano <gsarah6221@gmail.com>
2019-08-22 21:28:36 -0400

    added installation instructions

--- lassosum.sh
+++ lassosum.sh
@@ -2,10 +2,18 @@
 #run lassosum, stand-alone version (https://github.com/tshmak/lassosum/blob/master/lassosum_standalone.md#lassosum-standalone-version-for-linux) on prepared summary stats (base) and plink-formatted raw data (target) to get PRS
 #this script isn't pre-generalizable; go through and adjust values based on your base and target data and phenotype file(s)
 
+##INSTALL the lassosum R package and dependencies
+#refer to https://github.com/tshmak/lassosum
+#install.packages(c("RcppArmadillo", "data.table", "Matrix"), dependencies=TRUE)
+#library(devtools)
+#install_github("tshmak/lassosum")
+
 sumstats=$1 prepared sumstats file
 output=$2 #output prefix
 
-PATH=/net/snowwhite/home/sarahgag/R/x86_64-pc-linux-gnu-library/3.3/lassosum/:$PATH
+PATH=<path to lassosum R file>/lassosum/:$PATH
+#for instance, for me it is:
+#PATH=/net/snowwhite/home/sarahgag/R/x86_64-pc-linux-gnu-library/3.3/lassosum/:$PATH
 
 lassosum --data ${sumstats} \
         --chr dbSNP_CHR_b38 --pos dbSNP_BP_b38 \ #replace with appropriate column names from sumstats
