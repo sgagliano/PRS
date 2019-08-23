1fceebdd42825b79f7f650fa2ad5a299a8c31154
sgagliano <gsarah6221@gmail.com>
2019-08-22 21:21:23 -0400

    Create lassosum.sh

---
+++ lassosum.sh
@@ -0,0 +1,16 @@
+#Description
+#run lassosum, stand-alone version (https://github.com/tshmak/lassosum/blob/master/lassosum_standalone.md#lassosum-standalone-version-for-linux) on prepared summary stats (base) and plink-formatted raw data (target) to get PRS
+#this script isn't pre-generalizable; go through and adjust values based on your base and target data and phenotype file(s)
+
+sumstats=$1 prepared sumstats file
+output=$2 #output prefix
+
+PATH=/net/snowwhite/home/sarahgag/R/x86_64-pc-linux-gnu-library/3.3/lassosum/:$PATH
+
+lassosum --data ${sumstats} \
+        --chr dbSNP_CHR_b38 --pos dbSNP_BP_b38 \ #replace with appropriate column names from sumstats
+        --A1 A1 --A2 A2 --pval P --n 107620 --OR OR \ #replace with appropriate column names from sumstats
+        --test.bfile targetdataplinkprefix \
+        --LDblocks EUR.hg38 --pheno pheno.txt --covar covar.txt \ #first file has FID, IID and pheno; second has FID, IID and covariates
+        --ref refplinkprefix \
+        --splitvalidate --nthreads 2 --out ${output}
