e066143425ae7b2f01667bd5c39c74e2a4a0e4df
sgagliano <sgagliano@users.noreply.github.com>
2021-05-26 11:15:43 -0400

    add descriptions

--- Plotting/Violinplot-with1000G.R
+++ Plotting/Violinplot-with1000G.R
@@ -2,7 +2,7 @@ library(devtools)
 suppressPackageStartupMessages(require(RColorBrewer))
 suppressPackageStartupMessages(require(ggplot2))
 
-prs<-read.table("1000G_study.splitvalidate.results.txt", as.is=T, h=T) #lassosum results run on 1000G+study pop'ns 
+prs<-read.table("1000G_study.splitvalidate.results.txt", as.is=T, h=T) #PRS results
 pop<-read.table("1000g_inpsyght.txt", as.is=T, h=T) #Two column text file with headers IID and Group, listing sample id and corresponding category
 dat<-merge(prs, pop, by.x="IID", by.y="IID")
 
@@ -13,6 +13,8 @@ data_summary <- function(x) {
    return(c(y=m,ymin=ymin,ymax=ymax))
 }
 
+#x=<give name of Group/Phenotype header in `pop` dataframe>
+#y=<give name of PRS_score header in `prs` dataframe>
 tiff("violinplot.tiff", width=6.5, height=4.5, unit="in", res=300)
 ggplot(dat, aes(x=Group, y=best.pgs, color=Group)) + 
   geom_violin() +
